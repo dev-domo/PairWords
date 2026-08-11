d185c493a1121180849269694723090d2aca493f
heopaka <94223526+dev-domo@users.noreply.github.com>
2026-08-11 10:58:52 +0900

    chore: #1 기타 프로젝트 세팅

---
+++ PairWordsUITests/PairWordsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PairWordsUITestsLaunchTests.swift
+//  PairWordsUITests
+//
+//  Created by 더스틴 on 8/7/26.
+//
+
+import XCTest
+
+final class PairWordsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
