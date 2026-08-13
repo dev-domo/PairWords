c61b96f7479bf72bc27b25236973cb6c03346183
heopaka <94223526+dev-domo@users.noreply.github.com>
2026-08-13 11:55:17 +0900

    feat: #5 Firebase 설정

---
+++ PairWords/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  PairWords
+//
+//  Created by 더스틴 on 8/13/26.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
